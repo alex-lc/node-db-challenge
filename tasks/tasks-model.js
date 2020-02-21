d9ac0fb4a8383deba9eae314669a4eff8dea304e
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-02-21 12:11:02 -0600

    MVP Complete with Questions

--- tasks/tasks-model.js
+++ tasks/tasks-model.js
@@ -7,7 +7,9 @@ module.exports = {
 
 /* GET list of tasks */
 function getTasks() {
-    return db('tasks');
+    return db('tasks')
+        .join('projects', 'tasks.project_id', 'projects.id')
+        .select('tasks.*', 'projects.project_name', 'projects.project_desc')
 }
 
 /* POST to create a new task */
